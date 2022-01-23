522e8b520231779da6df5aa4a73b87c43e231e7f
VTAkbay <vahidturabakbay1@gmail.com>
2022-01-23 18:03:24 +0300

    contract ballot added

--- contracts/ballot.sol
+++ contracts/ballot.sol
@@ -2,5 +2,23 @@
 pragma solidity ^0.8.0;
 
 contract Ballot {
-    
+    struct Proposal {
+        bytes32 name;
+        uint256 voteCount;
+    }
+
+    address public chairperson;
+
+    Proposal[] public proposals;
+
+    constructor(bytes32[] memory proposalNames) {
+        chairperson = msg.sender;
+
+        for (uint256 i = 0; i < proposalNames.length; i++) {
+            // 'Proposal({...})' creates a temporary
+            // Proposal object and 'proposals.push(...)'
+            // appends it to the end of 'proposals'.
+            proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
+        }
+    }
 }
